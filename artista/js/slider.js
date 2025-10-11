86d25481e8e64bdcaeb93d3571114cff6e8e6312
Juan de Jesús Bautista García <L23350591@tuxtepec.tecnm.mx>
2025-10-10 23:25:22 -0600

    Add update artista

---
+++ artista/js/slider.js
@@ -0,0 +1,26 @@
+(function(){
+    const sliders = [...document.querySelectorAll('.news-body')];
+    const buttonNext = document.querySelector('#next');
+    const buttonBefore = document.querySelector('#before');
+    let value;
+
+    buttonNext.addEventListener('click', ()=>{
+        changePosition(1);
+    });
+
+    buttonBefore.addEventListener('click', ()=>{
+        changePosition(-1);
+    });
+
+    const changePosition = (add) => {
+        const currentNews = document.querySelector('.news-body-show').dataset.id;
+        value = Number(currentNews);
+        value += add;
+
+        sliders[Number(currentNews)-1].classList.remove('news-body-show');
+        if(value == sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length : 1;
+        }
+        sliders[value-1].classList.add('news-body-show');
+    }
+}) ();
